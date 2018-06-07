dc8df59d4455b5f0c2195920593b4c439b5e25ad
danielvportlandia <danielshelton90@gmail.com>
2018-06-06 17:41:26 -0700

    logout functionality added.

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -1,5 +1,7 @@
 import superagent from 'superagent';
 import * as routes from '../routes';
+import { deleteCookie } from '../utils/cookie';
+import { TOKEN_COOKIE_KEY } from '../constants';
 
 const setToken = token => ({
   type: 'TOKEN_SET',
@@ -10,10 +12,15 @@ const removeToken = () => ({
   type: 'TOKEN_REMOVE',
 });
 
+const logout = () => {
+  deleteCookie(TOKEN_COOKIE_KEY);
+  return removeToken();
+};
+
 const signupRequest = user => (store) => {
   return superagent.post(`${API_URL}${routes.SIGNUP_ROUTE}`)
     .send(user)
-    // .withCredentials()
+    .withCredentials()
     .then((response) => {
       return store.dispatch(setToken(response.text));
     });
@@ -22,10 +29,10 @@ const signupRequest = user => (store) => {
 const loginRequest = user => (store) => {
   return superagent.get(`${API_URL}${routes.LOGIN_ROUTE}`)
     .auth(user.username, user.password)
-    // .withCredentials()
+    .withCredentials()
     .then((response) => {
       return store.dispatch(setToken(response.text));
     });
 };
 
-export { setToken, removeToken, signupRequest, loginRequest };
+export { setToken, removeToken, logout, signupRequest, loginRequest };
